2796ef495da041e82400779badabec8cb7f9ba9b
Quang <your_email@gmail.com>
2026-04-15 01:40:15 +0700

    Initial commit: cau truc du an va firmware doc LM35 kenh A0

---
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -0,0 +1,17 @@
+const int sensorPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  float temperature = voltage * 100.0;
+
+  Serial.print("Nhiet do: ");
+  Serial.print(temperature);
+  Serial.println(" *C");
+
+  delay(1000);
+}
